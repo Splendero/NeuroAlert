672ac349f83fde0d29443cabb043e6f52a2e973e
Splendero <58837532+Splendero@users.noreply.github.com>
2025-10-05 14:56:30 -0400

    Initial Commit

---
+++ NeuroAlert/NeuroAlertApp.swift
@@ -0,0 +1,17 @@
+//
+//  NeuroAlertApp.swift
+//  NeuroAlert
+//
+//  Created by Spencer Osborn on 2025-10-05.
+//
+
+import SwiftUI
+
+@main
+struct NeuroAlertApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
